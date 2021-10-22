373b88554893219f041e9ad1fc486e8a865390d1
poojamb29 <91772967+poojamb29@users.noreply.github.com>
2021-10-22 13:54:33 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Logo from './logo.jpg';
+import Hamburger from './hamburger.png';
+import Blon from './Blonde-Female-Professional.jpg';
+import Like from './like.png';
+import Comm from './comment.png';
+
+
+
+function Index()
+{
+    return(
+        <div>
+            <div  className="demo1">
+           <div className="demo"> <img src={Hamburger} /></div>
+           <div> <img src={Logo}/></div>
+           <img className="demo2" src={Blon} />
+            </div>
+            
+            <center className="centr">
+            <div className="bodr">
+            <div><center><img  className="demo8" src={Blon}  /><sup id="john">by john</sup></center></div>
+             <center><h3 className="demo10">Thoughts on remote collaboration and<br/> medium team management at distance</h3></center>
+           <center> <span className="date">oct 22, 2021</span></center>
+            <div><center><img className="demo3" src={Blon} /></center></div>
+
+            <div  className="demo4">
+           <div className="demo5" > <img src={Like} /></div>
+           <div classname="demo7"> <img src={Comm}/><sup id="com"> 19 comments</sup></div>
+           <div><sup id="dom">Add a comments</sup><img className="demo6" src={Blon} /></div>
+            </div>
+           
+            </div>
+            </center>
+            
+
+
+
+            <center>
+            <div className="bodr">
+            <div><center><img  className="demo8" src={Blon}  /><sup id="john">by john</sup></center></div>
+             <center><h3 className="demo10">Thoughts on remote collaboration and<br/> medium team management at distance</h3></center>
+             <center> <span className="date">oct 22, 2021</span></center>
+            <div><center><img className="demo3" src={Blon} /></center></div>
+
+            <div  className="demo4">
+           <div className="demo5" > <img src={Like} /></div>
+           <div classname="demo7"> <img src={Comm}/><sup id="com">19 comments</sup></div>
+           <div><sup id="dom">Add a comments</sup><img className="demo6" src={Blon} /></div>
+            </div>
+            </div>
+            </center>
+            
+            <center>
+            <div className="bodr">
+            <div><center><img  className="demo8" src={Blon}  /><sup id="john">by john</sup></center></div>
+             <center><h3 className="demo10">Thoughts on remote collaboration and<br/> medium team management at distance</h3></center>
+             <center> <span className="date">oct 22, 2021</span></center>
+            <div><center><img className="demo3" src={Blon} /></center></div>
+
+            <div  className="demo4">
+           <div className="demo5" > <img src={Like} /></div>
+           <div classname="demo7"> <img src={Comm}/><sup id="com">19 comments</sup></div>
+           <div><sup id="dom">Add a comments</sup><img className="demo6" src={Blon} /></div>
+            </div>
+            </div>
+            </center>
+
+<center>
+            <div className="bodr">
+            <div><center><img  className="demo8" src={Blon}  /><sup id="john">by john</sup></center></div>
+             <center><h3 className="demo10">Thoughts on remote collaboration and<br/> medium team management at distance</h3></center>
+             <center> <span className="date">oct 22, 2021</span></center>
+            <div><center><img className="demo3" src={Blon} /></center></div>
+
+            <div  className="demo4">
+           <div className="demo5" > <img src={Like} /></div>
+           <div classname="demo7"> <img src={Comm}/><sup id="com">19 comments</sup></div>
+           <div><sup id="dom">Add a comments</sup><img className="demo6"  src={Blon} /></div>
+            </div>
+            </div>
+            </center>
+
+
+
+
+        </div>
+    )
+}
+export default Index;
+
